d1cf4c89920a7c0e699594d107e5458efe416e52
yuchenhsu0421 <166893751+yuchenhsu0421@users.noreply.github.com>
2024-10-09 01:32:10 -0500

    Update 0001.TwoSum.js

--- Leetcode/0001.TwoSum.js
+++ Leetcode/0001.TwoSum.js
@@ -1,1 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    // We create a Map to store each number and its index as we iterate through the array.
 
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        // We calculate its complement (the number needed to reach the target sum).
+
+        if (map.has(complement)) {
+            // We check if this complement exists in our map.
+            return [map.get(complement), i];
+            // If it does, we've found our pair and return their indices.
+        }
+
+        map.set(nums[i], i)
+        // If not, we add the current number and its index to the map.
+    }
+
+    return [];
+};
