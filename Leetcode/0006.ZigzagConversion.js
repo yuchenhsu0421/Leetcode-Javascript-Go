a7edc2faaed868b63a618c13b399ef093e8fb6f4
yuchenhsu0421 <166893751+yuchenhsu0421@users.noreply.github.com>
2024-10-22 01:33:00 -0500

    Update 0006.ZigzagConversion.js

--- Leetcode/0006.ZigzagConversion.js
+++ Leetcode/0006.ZigzagConversion.js
@@ -25,3 +25,23 @@ var convert = function(s, numRows) {
     return rows.join('');
     
 };
+
+
+// Iteration 1: P (currentRow = 0, step = 1)
+// P
+// Iteration 2: A (currentRow = 1, step = 1)
+// P
+// A
+// Iteration 3: Y (currentRow = 2, step = -1)
+// P
+// A
+// Y
+// Iteration 4: P (currentRow = 1, step = -1)
+// P
+// AP
+// Y
+// Iteration 5: A (currentRow = 0, step = 1)
+// PA
+// AP
+// Y
+// ... and so on
